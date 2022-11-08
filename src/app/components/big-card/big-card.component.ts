7301a4d20cdf77387edf5ecfa3e82a2e7e222df3
Denis Souza <denis.souza@ulteriorti.com.br>
2022-11-07 22:09:05 -0300

    Ajuste de responsividade

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.css']
+  styleUrls: ['./big-card.component.css','./big-card.component.largescreen.css']
 })
 export class BigCardComponent implements OnInit {
 
