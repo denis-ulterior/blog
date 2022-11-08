06ec605293b11fd21d884464aef7b53cefa085a0
Denis Souza <denis.souza@ulteriorti.com.br>
2022-11-08 18:37:27 -0300

    Criada página e configurada rota

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -11,7 +11,7 @@ export class BigCardComponent implements OnInit {
   @Input()
   cardTitle:string = ''
   @Input()
-  cardDescription:string = ''
+  lorem:string = ''
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/data/mockData.ts
@@ -0,0 +1,8 @@
+export const mockData = [
+    {
+        "id":1,
+        "photo":"linkphoto",
+        "title":"Novo post",
+        "description":"texto"
+    }
+]
