9f340f0b650c9bdefc495fc7aee5fee0cf4d3e1f
Denis Souza <denis.souza@ulteriorti.com.br>
2022-11-08 19:33:21 -0300

    Projeto base concluido

--- src/app/data/mockData.ts
+++ src/app/data/mockData.ts
@@ -1,6 +1,24 @@
 export const mockData = [
     {
-        "id":1,
+        "id":"1",
+        "photo":"linkphoto",
+        "title":"Novo post",
+        "description":"texto"
+    },
+    {
+        "id":"2",
+        "photo":"linkphoto",
+        "title":"Novo post",
+        "description":"texto"
+    },
+    {
+        "id":"3",
+        "photo":"linkphoto",
+        "title":"Novo post",
+        "description":"texto"
+    },
+    {
+        "id":"4",
         "photo":"linkphoto",
         "title":"Novo post",
         "description":"texto"
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { mockData } from 'src/app/data/mockData';
 
 @Component({
   selector: 'app-content',
@@ -9,9 +11,23 @@ export class ContentComponent implements OnInit {
   photoCover:string=''
   contentTitle:string=''
   contentDescription:string = ''
-  constructor() { }
+  private id:string | null = '0'
+  constructor(
+    private route:ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(
+      valor =>this.id = valor.get("id")
+    )
+    this.setComponentValues(this.id)
+  }
+  setComponentValues(id:string | null){
+    const result = mockData.filter(article => article.id == id)[0]
+    this.contentTitle = result.title
+    this.contentDescription = result.description
+    this.photoCover = result.photo
+    
   }
 
 }
